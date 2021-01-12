835b07bf73505f60acc627d54dd27ce554566f63
pragathis122007 <74417739+pragathis122007@users.noreply.github.com>
2021-01-12 17:19:44 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -13,15 +13,31 @@ class Box {
       World.add(world, this.body);
     }
     display(){
-      var pos =this.body.position;
-      var angle = this.body.angle;
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      fill(255);
-      rect(0, 0, this.width, this.height);
-      pop();
+      if(this.body.speed<3){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(255);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+      else {
+        World.remove(world, this.body);
+        push ();
+        this.Visiblity = this.Visiblity - 5;
+        tint (255,this.Visiblity);
+        image(this.image,this.body.position.x,this.body.position.y,50,50);
+        pop ();
+      }
+    
+    }
+    score(){
+      if (this.Visiblity < 0 && this.Visiblity > -1005){
+        score = 500;
+      }
     }
   };
   
--- sketch.js
+++ sketch.js
@@ -5,14 +5,20 @@ const Constraint = Matter.Constraint;
 
 var stand;
 var polygon1;
+
 var block1,block2,block3,block4,block5,block6,block7;
 var block8,block9,block10,block11,block12;
 var block13,block14,block15;
 var block16;
+
 var slingshot1;
 
+//var backgroundImg;
+//var bg = "daybackground.png";
+var score = 0;
+
 function preload(){
- 
+   //  getBackgroundImg();
 }
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -47,11 +53,21 @@ function setup(){
     polygon1 = new Polygon(50,250);
     slingshot1 = new SlingShot(polygon1.body,{x:50,y:200});
 
+  
+
     Engine.run(engine);
 }
 
 function draw(){
-    background("white");
+   // if(backgroundImg)
+       //  background(backgroundImg);
+
+    background("red");
+
+  //  noStroke();
+    //textSize(35)
+    //fill("white")
+    text ("SCORE : "+score,750,40);
 
     Engine.update(engine);
     ground.display();
@@ -59,29 +75,50 @@ function draw(){
     polygon1.display();
 
     block1.display();
+    block1.score();
     block2.display();
+    block2.score();
     block3.display();
+    block3.score();
     block4.display();
+    block4.score();
     block5.display();
+    block5.score();
     block6.display();
+    block6.score();
     block7.display();
+    block7.score();
 
     block8.display();
+    block8.score();
     block9.display();
+    block9.score();
     block10.display();
+    block10.score();
     block11.display();
+    block11.score();
     block12.display();
+    block12.score();
 
     block13.display();
+    block13.score();
     block14.display();
+    block14.score();
     block15.display();
+    block15.score();
 
     block16.display();
+    block16.score();
 
     slingshot1.display();
     
+  
+
     drawSprites();
+
+  
 }
+
 
 function mouseDragged(){
     Matter.Body.setPosition(polygon1.body, {x: mouseX , y: mouseY});
@@ -93,3 +130,28 @@ function mouseReleased(){
     Matter.Body.setPosition(polygon1.body, {x: 650 , y: 300});
       slingshot1.fly();
 }
+
+function keyPressed(){
+    if(keyCode === 32){
+     //  Matter.Body.setPosition(polygon1.body, {x: 50 , y: 250});
+     slingshot1.attach(polygon1.body);
+    }
+}
+
+//async function getBackgroundImg(){
+ //   var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    //var responseJSON = await response.json();
+
+    //var datetime = responseJSON.datetime;
+   // var hour = datetime.slice(11,13);
+    
+  //  if(hour>=0600 && hour<=1900){
+   //     bg = "daybackground.png";
+   // }
+   // else{
+    //    bg = "nightbackground.png";
+  //  }
+
+   // backgroundImg = loadImage(bg);
+  //  console.log(backgroundImg);
+//}
